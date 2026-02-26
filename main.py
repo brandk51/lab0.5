4eac71faf5ef08b64f7e28e4272f79ca886a7ede
brandk51 <bk51@sunyorange.edu>
2026-02-25 23:18:16 -0500

    Final main.py
    Completed version

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from my_functions import get_plaintext, check_equals, get_alpha_lst
 #TODO: (Input)   Assign data to the variables.
 alpha_lst = get_alpha_lst() #list of letter A - Z
 plaintext = get_plaintext() #generates 3 random letters
-print(plaintext)
 #TODO: (Process) Manipulate the string.
 jointext = ' '.join(plaintext) #seperates letters by a space
 splittext = jointext.split(' ') #splits text
@@ -17,28 +16,21 @@ avalue = alpha_lst.index(first)  #first value
 bvalue = alpha_lst.index(second) #second value
 cvalue = alpha_lst.index(third)  #third value
 
-anewvalue = avalue - 3
+anewvalue = avalue - 3 #reduces value by 3
 bnewvalue = bvalue - 3
 cnewvalue = cvalue - 3
 
-afinalvalue = alpha_lst[anewvalue]
+afinalvalue = alpha_lst[anewvalue] #obtains new letter from the list
 bfinalvalue = alpha_lst[bnewvalue]
 cfinalvalue = alpha_lst[cnewvalue]
 
-listvalue = afinalvalue + bfinalvalue + cfinalvalue
-completevalue = ' '.join(listvalue)
+listvalue = afinalvalue + bfinalvalue + cfinalvalue #combines for display purposes
+
+completevalue = ' '.join(listvalue)         #converts it into a proper list
 completevalue = completevalue.split(' ')
 ciph_text = str(completevalue)
 
-exp, act = listvalue, plaintext
 #TODO: (Output)  Display the asserted cipher text.
 print(ciph_text)
-print(exp)
-print(act)
-
-#assert check_equals(exp,act)
-'''
-# Ready for the extra credit?
-exp, act = 'DEF', 'ABC'
-assert check_equals(exp,act,False), "this should fail"
-'''
+assert check_equals(plaintext,listvalue) #Checks if the Cipher worked correctly
+print ("Test Completed")
