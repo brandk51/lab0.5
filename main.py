61a39c575a54cabbd5c6c79b0cc13a1b6443aa57
brandk51 <bk51@sunyorange.edu>
2026-02-25 20:31:12 -0500

    Create main.py
    starting point

---
+++ main.py
@@ -0,0 +1,14 @@
+from my_functions import get_plaintext, check_equals, get_alpha_lst
+# DO NOT UPDATE CODE ABOVE THIS LINE
+
+#TODO: (Input)   Assign data to the variables.
+alpha_lst = get_alpha_lst()
+
+#TODO: (Process) Manipulate the string.
+#TODO: (Output)  Display the asserted cipher text.
+
+'''
+# Ready for the extra credit?
+exp, act = 'DEF', 'ABC'
+assert check_equals(exp,act,False), "this should fail"
+'''
