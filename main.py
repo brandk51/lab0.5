c9ee3534a590c6795053ec1f6de1baf054f2521b
brandk51 <bk51@sunyorange.edu>
2026-02-25 22:15:36 -0500

    Update2 main.py
    completed TODO section

--- main.py
+++ main.py
@@ -4,9 +4,30 @@ from my_functions import get_plaintext, check_equals, get_alpha_lst
 #TODO: (Input)   Assign data to the variables.
 alpha_lst = get_alpha_lst() #list of letter A - Z
 plaintext = get_plaintext() #generates 3 random letters
+
 #TODO: (Process) Manipulate the string.
-jointext = ' '.join(plaintext)
+jointext = ' '.join(plaintext) #seperates letters by a space
 print(jointext)
+splittext = jointext.split(' ') #splits text
+print(splittext)
+
+first   = splittext[0]   #finds first element
+second  = splittext[1]   #finds second element
+third   = splittext[2]   #finds third element
+
+avalue = alpha_lst.index(first)  #first value 
+bvalue = alpha_lst.index(second) #second value
+cvalue = alpha_lst.index(third)  #third value
+
+anewvalue = avalue - 3
+bnewvalue = bvalue - 3
+cnewvalue = cvalue - 3
+
+afinalvalue = alpha_lst[anewvalue]
+bfinalvalue = alpha_lst[bnewvalue]
+cfinalvalue = alpha_lst[cnewvalue]
+print(afinalvalue + bfinalvalue + cfinalvalue)
+
 #TODO: (Output)  Display the asserted cipher text.
 
 '''
