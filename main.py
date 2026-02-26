3e79e0e78ac3e5b23314f5e87265f734f4b68190
brandk51 <bk51@sunyorange.edu>
2026-02-25 20:48:41 -0500

    Update main.py
    completed input and completed the join for TODO

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from my_functions import get_plaintext, check_equals, get_alpha_lst
 # DO NOT UPDATE CODE ABOVE THIS LINE
 
 #TODO: (Input)   Assign data to the variables.
-alpha_lst = get_alpha_lst()
-
+alpha_lst = get_alpha_lst() #list of letter A - Z
+plaintext = get_plaintext() #generates 3 random letters
 #TODO: (Process) Manipulate the string.
+jointext = ' '.join(plaintext)
+print(jointext)
 #TODO: (Output)  Display the asserted cipher text.
 
 '''
